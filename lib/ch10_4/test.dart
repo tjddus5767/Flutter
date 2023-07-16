7001b71dffec174bce39c5595d7403579d5aad20
tjddus <rlatjddus5767@naver.com>
2023-07-16 17:42:43 +0900

    10-4 test.dart

--- lib/ch10_4/test.dart
+++ lib/ch10_4/test.dart
@@ -14,64 +14,88 @@ class MyApp7 extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Practice',
-          ),
-          centerTitle: true,
-          flexibleSpace: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: <Color>[
-                  Colors.red,
-                  Colors.blue,
-                  Colors.black,
-                ],
+          appBar: AppBar(
+            title: const Text(
+              'Practice',
+            ),
+            centerTitle: true,
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[
+                    Colors.red,
+                    Colors.blue,
+                    Colors.black,
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: SingleChildScrollView(
+          body: SingleChildScrollView(
             scrollDirection: Axis.horizontal,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Column(
               children: <Widget>[
-                Image.asset(
-                  'images/Insta1.png',
-                  width: 50,
-                  height: 50,
+                SizedBox(
+                  height: 300,
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        color: Colors.red,
+                        width: 100,
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.orange,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.amber,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                const SizedBox(
-                  width: 10,
+                Container(
+                  color: Colors.green,
+                  height: 300,
+                  child: Row(
+                    children: <Widget>[
+                      Image.asset(
+                        'images/Insta1.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                      Image.asset(
+                        'images/insta2.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                      Image.asset(
+                        'images/insta3.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                      const Spacer(),
+                      Image.asset(
+                        'images/insta4.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                    ],
+                  ),
                 ),
-                Image.asset(
-                  'images/insta2.png',
-                  width: 50,
-                  height: 50,
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Image.asset(
-                  'images/insta3.png',
-                  width: 50,
-                  height: 50,
-                ),
-                const Spacer(),
-                Image.asset(
-                  'images/insta4.png',
-                  width: 50,
-                  height: 50,
-                ),
+                Container(
+                  color: Colors.blue,
+                  height: 100,
+                )
               ],
             ),
-          ),
-        ),
-      ),
+          )),
     );
   }
 }
