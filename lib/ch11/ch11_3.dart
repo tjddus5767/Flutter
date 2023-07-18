f3a02e64e251e420fbe0136714a843b61872a94d
tjddus <rlatjddus5767@naver.com>
2023-07-18 18:00:55 +0900

    ch11-3

---
+++ lib/ch11/ch11_3.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ch11_3());
+}
+
+class ch11_3 extends StatelessWidget {
+  const ch11_3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                'Practice',
+              ),
+              centerTitle: true,
+              flexibleSpace: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[
+                      Colors.red,
+                      Colors.blue,
+                      Colors.black,
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            body: const TestScreen()));
+  }
+}
+
+class TestScreen extends StatefulWidget {
+  const TestScreen({super.key});
+
+  @override
+  MyFormState createState() => MyFormState();
+}
+
+class MyFormState extends State<TestScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String? firstName;
+  String? lastName;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: [
+        const Text('Form Test'),
+        Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'FirstNdame'),
+                validator: (value) {
+                  if (value?.isEmpty ?? false) {
+                    return 'Please enter first name';
+                  }
+                  return null;
+                },
+                onSaved: (String? value) {
+                  firstName = value;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'LastName'),
+                validator: (value) {
+                  if (value?.isEmpty ?? false) {
+                    return 'Please enter last name';
+                  }
+                  return null;
+                },
+                onSaved: (String? value) {
+                  lastName = value;
+                },
+              ),
+            ],
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (_formKey.currentState?.validate() ?? false) {
+              _formKey.currentState?.save();
+              print('firstName: $firstName, lastName : $lastName');
+            }
+          },
+          child: const Text('submit'),
+        )
+      ],
+    );
+  }
+}
